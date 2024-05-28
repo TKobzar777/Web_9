033fe5f5b40cb185fb2f41a6a34cfe5706041b7e
Tetiana Kobzar <140975446+TKobzar777@users.noreply.github.com>
2024-05-28 18:42:49 +0100

    Update models.py

--- models.py
+++ models.py
@@ -2,7 +2,7 @@
 from bson import json_util
 from mongoengine import connect, Document, StringField, ReferenceField, ListField, CASCADE, EmailField, BooleanField
 
-uri = "mongodb+srv://user1:567234@tetianakobzar.2kizr3f.mongodb.net/?retryWrites=true&w=majority&appName=TetianaKobzar"
+uri = "mongodb+srv://user1:******@tetianakobzar.2kizr3f.mongodb.net/?retryWrites=true&w=majority&appName=TetianaKobzar"
 
 connect(db="TetianaKobzar", host=uri)
 
